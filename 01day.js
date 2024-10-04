a9e57d2481f552c957daafc074a772705f2bdcba
Sharad singh <singhsharad095@gmail.com>
2024-10-04 14:50:38 +0530

    1st

---
+++ 01day.js
@@ -0,0 +1,31 @@
+let age=19;
+
+switch(age) {
+    case 21:
+        console.log("i am 21");
+        break;
+
+        case 20:
+            console.log("i am20");
+            break;
+
+        case 19:
+        console.log("i am 19");
+        break;
+        
+        default:
+            console.log("whatever");
+            break;
+}
+
+for(let i=1; i<=10;i=i+1){
+    console.log("my name is sharad")
+
+}
+ let girls=99;
+ girls<100 ?console.log("yes") : console.log("no");
+
+ for (let i = 1; i <= 10; i++) {
+    console.log(`2 x ${i} = ${2 * i}`);
+}
+clear
